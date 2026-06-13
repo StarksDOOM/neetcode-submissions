aa0d78536b3e1dfa64e8d5f075a997cb202d1523
Gamaliel Fulgencio <drzfinest87@gmail.com>
2026-06-13 12:14:00 -0400

    Add: find-target-in-rotated-sorted-array - submission-1

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-1.ts
@@ -0,0 +1,42 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums: number[], target: number): number {
+        //define left and right points
+        let left = 0;
+        let right = nums.length - 1;
+
+        while (left <= right ) {
+            //lets define the middle
+            let mid = Math.floor((left + right) / 2);
+
+            //we found the jackpot!
+            if( nums[mid] === target ) {
+                return mid;
+            } else if (nums[left] <= nums[mid]) { // Left side is sorted
+                //left side is sorted
+                if (target >= nums[left] && target < nums[mid]) {
+                    right = mid - 1; // Target is in the left half!
+                } else {
+                    left = mid + 1;  // Target is in the right half!
+                }
+
+            } else {
+
+                //right side is sorted
+                if (target <= nums[right] && target > nums[mid]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }   
+            }
+
+        }
+
+        //target was never found
+        return -1;
+    }
+}
