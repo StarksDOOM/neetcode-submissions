7d7e356770f3bd3c90b01e9b75f03ca6d14ce0c1
Gamaliel Fulgencio <drzfinest87@gmail.com>
2026-06-14 11:36:19 -0400

    Add: time-based-key-value-store - submission-1

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-1.ts
@@ -0,0 +1,65 @@
+class TimeMap {
+
+    keyStore: Map<string, { value: string; timestamp: number }[]>;
+
+    constructor() {
+        this.keyStore = new Map();
+    }
+
+    /**
+     * @param {string} key
+     * @param {string} value
+     * @param {number} timestamp
+     * @return {void}
+     */
+    set(key: string, value: string, timestamp: number): void {
+        //we check if the key exists already... if it doesnt set an empty array
+        if( !this.keyStore.has(key) ) this.keyStore.set(key, []);
+
+        //then we add the key into the array
+        this.keyStore.get(key).push({ value, timestamp });
+    }
+
+    /**
+     * @param {string} key
+     * @param {number} timestamp
+     * @return {string}
+     */
+    get(key: string, timestamp: number): string {
+
+        //if there is no result, just return empty string
+        if( !this.keyStore.has(key) ) return "";
+
+        //store the history from the keystore
+        const history = this.keyStore.get(key);
+
+        //backwards loop to find the newest item
+        //for( let i = history.length -1; i >= 0; i--) {
+        //    
+        //    if (history[i]['timestamp'] <= timestamp) {
+        //        return history[i]['value'];
+        //    }
+        //}
+
+        //this is the binary search version
+        let left = 0;
+        let right = history.length - 1;
+        let res = "";
+
+        while( left <= right ) {
+            let mid = Math.floor(left + (right - left) / 2)
+
+            if( history[mid]['timestamp'] <= timestamp ) {
+                res = history[mid]['value'];
+                left = mid + 1;
+            } else if ( history[mid]['timestamp'] > timestamp) {
+               right = mid - 1; 
+            }
+
+        }
+
+        //fallback if the target timestamp is older than our earliest entry
+        return res;
+
+    }
+}
